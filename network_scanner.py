9ba1f60538cfbbf59fd3f94b80932a7af2c3ab20
Luis Ramirez <sdaewa22@gmail.com>
2021-05-02 19:24:49 +0200

    refactor

--- network_scanner.py
+++ network_scanner.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import scapy.all as scapy
-import optparse
+import argparse
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option('-t', '--Target', dest = 'ip_target', help = 'IP Address in subnet to scan')
-    (options, arguments) = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--Target', dest = 'ip_target', help = 'IP Address in subnet to scan')
+    options = parser.parse_args()
 
     if not options.ip_target:
         print('[-] Specify a range')
         parser.exit()
-
 
     return options.ip_target
 
@@ -20,7 +19,6 @@ def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
     broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
-
     answ_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
 
     clients_list = []
@@ -28,6 +26,7 @@ def scan(ip):
     for element in answ_list:
         client_dictionary = {'ip': element[0].psrc, 'mac': element[0].hwsrc}
         clients_list.append(client_dictionary)
+        
     return clients_list
 
 
