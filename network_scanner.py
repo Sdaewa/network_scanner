1b9e6f2cc25da18aeccffb745f531d139c1866eb
Luis Ramirez <sdaewa22@gmail.com>
2021-05-02 14:00:48 +0200

    sending and receiving packs answers

--- network_scanner.py
+++ network_scanner.py
@@ -7,7 +7,7 @@ def scan(ip):
     broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
     arp_req_broad = broadcast/arp_request
     answ, unansw = scapy.srp(arp_req_broad, timeout = 1)
-    print(unansw.summary())
+
 
 
 scan('10.0.2.1/24')
