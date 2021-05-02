5381d9ef11194d86e8620ee15f22ca5c54af5aac
Luis Ramirez <sdaewa22@gmail.com>
2021-05-02 19:14:53 +0200

    refactor

--- network_scanner.py
+++ network_scanner.py
@@ -1,20 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import scapy.all as scapy
 import optparse
-
-
-def scan(ip):
-    arp_request = scapy.ARP(pdst = ip)
-    broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
-    arp_req_broad = broadcast/arp_request
-
-    answ_list = scapy.srp(arp_req_broad, timeout = 1, verbose = False)[0]
-
-    print('IP\t\t\tMAC Address\n------------------------------------------')
-
-    for element in answ_list:
-        print(element[1].psrc + '\t\t' + element[1].hwsrc)
-
 
 
 def get_arguments():
@@ -30,6 +16,30 @@ def get_arguments():
     return options.ip_address
 
 
+def scan(ip):
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast/arp_request
+
+    answ_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
+
+    clients_list = []
+
+    for element in answ_list:
+        client_dictionary = {'ip': element[0].psrc, 'mac': element[0].hwsrc}
+        clients_list.append(client_dictionary)
+    return clients_list
+
+
+
+def print_result(results_list):
+    print('IP\t\t\tMAC Address\n------------------------------------------')
+    for client in results_list:
+        print(client['ip'] + '\t\t' + client['mac'])
+
+
+
 
 range = get_arguments()
-scan(range)
+scan_result = scan(range)
+print_result(scan_result)
