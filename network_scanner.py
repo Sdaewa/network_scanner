5589a25119154acc1ef07264aea68f67d7f64e36
Luis Ramirez <sdaewa22@gmail.com>
2021-05-02 13:46:09 +0200

    arp request/mac destination

--- network_scanner.py
+++ network_scanner.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
 
 
 scan('10.0.2.1/24')
